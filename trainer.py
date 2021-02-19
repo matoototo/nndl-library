96f8b841acb92f27efb54be13c6911945312ce45
matoototo <furrowedsip@gmail.com>
2021-02-19 14:34:54 +0100

    Report acc/loss train statistics.

--- trainer.py
+++ trainer.py
@@ -35,29 +35,38 @@ class Trainer:
 			delta = (layers[l].weights.T @ deltas[-1]) * sigmoid_prime(w_in) * layers[l-1].dropout_mask
 			deltas.append(delta)
 			nablas_w.append(outer(act, delta).T)
-		return nablas_w[::-1], deltas[::-1]
+		return nablas_w[::-1], deltas[::-1], self.stats(a[-1], y)
 
 	def train_batch(self, batch_x, batch_y):
 		nablas_w = [zeros_like(l.weights) for l in self.network.layers]
 		nablas_b = [zeros_like(l.biases) for l in self.network.layers]
+		correct = loss = 0
 		for x, y in zip(batch_x, batch_y):
-			d_w, d_b = self.backprop(x, y)
+			d_w, d_b, (c, l) = self.backprop(x, y)
+			correct += c
+			loss += l
 			nablas_w = [a+b for a, b in zip(d_w, nablas_w)]
 			nablas_b = [a+b for a, b in zip(d_b, nablas_b)]
 		self.gradient_descent(self.div_arrs(nablas_w, len(batch_x)), self.div_arrs(nablas_b, len(batch_x)))
+		return (correct, loss)
 
 	def SGD(self, epochs, report):
 		batches_x = [array(self.x[i : i + self.batchsize]) for i in range(0, len(self.x), self.batchsize)]
 		batches_y = [array(self.y[i : i + self.batchsize]) for i in range(0, len(self.y), self.batchsize)]
 		batches_x, batches_y = self.shuffle_together(batches_x, batches_y)
-		info = []
+		info = {"train": [], "test": []}
 		for epoch in range(epochs):
 			i = 0
+			loss = correct = 0
 			for batch_x, batch_y in zip(batches_x, batches_y):
-				self.train_batch(batch_x, batch_y)
+				c, l = self.train_batch(batch_x, batch_y)
+				correct += c
+				loss += l
 				report(i, self.batchsize, len(self.x))
 				i += 1
-			info.append(self.evaluate(epoch))
+			info["train"].append({"epoch": epoch, "acc": 100*correct/len(self.x), "loss": loss/len(self.x)})
+			info["test"].append(self.evaluate(epoch))
+			self.report_stats(info)
 		return info
 
 	def evaluate(self, epoch):
@@ -69,11 +78,21 @@ class Trainer:
 			correct += argmax(a[-1])==argmax(y)
 		acc = 100*correct / len(self.x_test)
 		loss = loss / len(self.x_test)
-		print("\x1b[2K") # clear line
-		print(f"Epoch {epoch+1}")
-		print("Accuracy:", acc, "%")
-		print("Loss:", round(loss, 4))
 		return {"epoch": epoch+1, "acc": acc, "loss": loss}
+
+	def stats(self, x, y):
+		return (argmax(x)==argmax(y), self.loss.loss(x, y))
+
+	@staticmethod
+	def report_stats(info):
+		test = info["test"][-1]
+		train = info["train"][-1]
+		print("\x1b[2K") # clear line
+		print("Epoch", test["epoch"]+1)
+		print("Test Accuracy:", test["acc"], "%")
+		print("Test Loss:", round(test["loss"], 4))
+		print("Train Accuracy:", train["acc"], "%")
+		print("Train Loss:", round(train["loss"], 4))
 
 	@staticmethod
 	def report_train(i, batchsize, length):

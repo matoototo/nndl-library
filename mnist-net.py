1502afecf86c483f38317ead169a30d1af1d9e43
matoototo <furrowedsip@gmail.com>
2021-02-20 22:36:06 +0100

    Data augmentation (rotation).
    The network was of the format: [784, 30, 10].
    The base dataset was 1000 images (blue), augmented by -5, +5 degree rotations to 3000 images (green).

--- mnist-net.py
+++ mnist-net.py
@@ -1,9 +1,10 @@
 from network import Network, Layer
 from trainer import Trainer
 from functions import sigmoid, MSE, CrossEntropy, softmax, L2Reg
-from numpy import array
+from numpy import array, reshape
 from mnist import MNIST
 from matplotlib import pyplot
+from scipy.ndimage import rotate
 
 # An example using the nndl library on the MNIST dataset.
 
@@ -15,6 +16,18 @@ def one_hot(labels):
 		ys.append(array(y))
 	return array(ys)
 
+def augment(images, labels, start = 10, end = 20, inc = 10):
+	aug_images = []
+	aug_labels = []
+	for image, label in zip(images, labels):
+		aug_images.append(image)
+		aug_labels.append(label)
+		for i in range(start, end, inc):
+			rotated = rotate(reshape(image, (28, 28)), i, reshape=False)
+			aug_images.append(reshape(rotated, (784)))
+			aug_labels.append(label)
+	return (array(aug_images), array(aug_labels))
+
 mnist = MNIST("./mnist")
 mnist.gz = True
 images, labels = mnist.load_training()
@@ -25,16 +38,23 @@ images_test = array(images_test) / 255.
 labels = one_hot(labels)
 labels_test = one_hot(labels_test)
 
+aug_images, aug_labels = augment(images[0:1000], labels[0:1000], start=-5, end=15, inc=10)
+
 colours = ["b", "g", "r", "c", "m"]
 
 l2 = L2Reg(0.5)
-net1 = Network([Layer(784, 50, sigmoid, l2, 0.0), Layer(50, 10, softmax, l2)])
-nets = [net1]
-for i in range(1):
-	trainer = Trainer(nets[i], CrossEntropy, 0.5, 10, images, labels, images_test[0:1000], labels_test[0:1000])
-	data = trainer.SGD(100, tr_acc = False, tr_loss = False)
+net1 = Network([Layer(784, 30, sigmoid, l2, 0.0), Layer(30, 10, softmax, l2)])
+l2 = L2Reg(1.5)
+net2 = Network([Layer(784, 30, sigmoid, l2, 0.0), Layer(30, 10, softmax, l2)])
+nets = [net1, net2]
+for i in range(2):
+	if i == 0: trainer = Trainer(nets[i], CrossEntropy, 0.5, 10, images[0:1000], labels[0:1000], images_test[0:2500], labels_test[0:2500])
+	if i == 1: trainer = Trainer(nets[i], CrossEntropy, 0.5, 10, aug_images[0:3000], aug_labels[0:3000], images_test[0:2500], labels_test[0:2500])
+	data = trainer.SGD(300, tr_acc = False, tr_loss = False)
 	y_points = [point["epoch"] for point in data["test"]]
 	x_points = [point["acc"] for point in data["test"]]
 	pyplot.plot(y_points, x_points, colours[i])
+	x_points = [point["acc"] for point in data["train"]]
+	pyplot.plot(y_points, x_points, colours[i]+"--")
 
 pyplot.show()
